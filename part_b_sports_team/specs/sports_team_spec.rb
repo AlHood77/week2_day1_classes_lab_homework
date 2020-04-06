6d43c5e9e8abc93cba463b75612d5016adb75582
Alan <alanhood77@gmail.com>
2020-04-06 15:36:59 +0100

    Committ after lab session finished

--- part_b_sports_team/specs/sports_team_spec.rb
+++ part_b_sports_team/specs/sports_team_spec.rb
@@ -4,3 +4,25 @@ require_relative('../sports_team')
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
 class TestSportsTean < MiniTest::Test
+
+    def setup
+        @team = SportsTeam.new("Raith Rovers", ["Dennis", "Alan", "Dave"], "Eugene")
+    end
+
+    def test_get_team_name
+        assert_equal("Raith Rovers", @team.team_name)
+    end
+
+    def test_get_player_name
+        assert_equal(["Dennis", "Alan", "Dave"], @team.players)
+    end
+
+    def test_get_coach_name
+        assert_equal("Eugene", @team.coach)
+    end
+
+    def test_update_coach_name
+        @team.coach = "Rory"
+        assert_equal("Rory", @team.coach)
+    end
+end
